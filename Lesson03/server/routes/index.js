d773f88a38c36c5978bd1357c8f95210723edca8
buimyy0409 <buimyy2909@gmail.com>
2023-12-01 13:34:50 +0700

    update bt

--- Lesson03/server/routes/index.js
+++ Lesson03/server/routes/index.js
@@ -5,7 +5,9 @@ const userRouter = require('./user.route');
 const postRouter = require('./post.route');
 const loggerMdw = require('./../middlewares/logger.mdw')
 const {requireAPIKey} = require('./../middlewares/apiKey.mdw')
+const methodMdw = require('./../middlewares/method.mdw')
 router.use(loggerMdw)
+router.use(methodMdw)
 router.use('/user', userRouter)
 router.use('/post', requireAPIKey, postRouter)
 
